04ebc0a9a68c9e1681a796a378eed104e704ce19
Gerry Agbobada <gagbobada+git@gmail.com>
2018-04-05 01:50:35 +0200

    Add tests for range query, ready for when logic is necessary

--- test/test_kdtree.cpp
+++ test/test_kdtree.cpp
@@ -12,6 +12,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <algorithm>
 #include <Eigen/Dense>
 #include <memory>
 #include "catch.hpp"
@@ -52,5 +53,76 @@ TEST_CASE("Insertion into a tree", "[kdtree][insert]") {
     SECTION("Test the data accessors from the tree") {
         REQUIRE(tree.get_root()->x() == Approx(ent_1->get_pos()(0)));
         REQUIRE(tree.get_root()->y() == Approx(ent_1->get_pos()(1)));
+    }
+}
+
+TEST_CASE("Range Query in the kdtree", "[kdtree][range_query]") {
+    KDTree<Entity> tree;
+    std::shared_ptr<Entity> ent_1(
+        std::move(Entity::makeEntity(Entity::Type::ANT, 50.0, 50.1)));
+    std::shared_ptr<Entity> ent_2(
+        std::move(Entity::makeEntity(Entity::Type::FOOD, 25.0, 55.2)));
+    std::shared_ptr<Entity> ent_3(
+        std::move(Entity::makeEntity(Entity::Type::ANT, 75.0, 22.3)));
+    std::shared_ptr<Entity> ent_4(
+        std::move(Entity::makeEntity(Entity::Type::FOOD, 60.0, 21.0)));
+    std::shared_ptr<Entity> ent_5(
+        std::move(Entity::makeEntity(Entity::Type::ANT, 58.75, 37.3)));
+    std::shared_ptr<Entity> ent_6(
+        std::move(Entity::makeEntity(Entity::Type::FOOD, 19.89, 13.9)));
+    std::shared_ptr<Entity> ent_7(
+        std::move(Entity::makeEntity(Entity::Type::ANT, 3.1, 67.2)));
+    std::shared_ptr<Entity> ent_8(
+        std::move(Entity::makeEntity(Entity::Type::FOOD, 75.25, 1.2)));
+    std::shared_ptr<Entity> ent_9(
+        std::move(Entity::makeEntity(Entity::Type::ANT, 24.98, 39.0)));
+    tree.insert(ent_1);
+    tree.insert(ent_2);
+    tree.insert(ent_3);
+    tree.insert(ent_4);
+    tree.insert(ent_5);
+    tree.insert(ent_6);
+    tree.insert(ent_7);
+    tree.insert(ent_8);
+    tree.insert(ent_9);
+
+    SECTION("Test a successful range query on an entity") {
+        Eigen::Vector2d center(60.0, 21.0);
+        float radius = 15.0;
+        auto result = tree.norm1_range_query(center, radius);
+
+        REQUIRE(std::find(result.begin(), result.end(), ent_1) == result.end());
+        REQUIRE(std::find(result.begin(), result.end(), ent_2) == result.end());
+        // REQUIRE(std::find(result.begin(), result.end(), ent_3) != result.end());
+        // REQUIRE(std::find(result.begin(), result.end(), ent_4) != result.end());
+        REQUIRE(std::find(result.begin(), result.end(), ent_5) == result.end());
+        REQUIRE(std::find(result.begin(), result.end(), ent_6) == result.end());
+        REQUIRE(std::find(result.begin(), result.end(), ent_7) == result.end());
+        REQUIRE(std::find(result.begin(), result.end(), ent_8) == result.end());
+        REQUIRE(std::find(result.begin(), result.end(), ent_9) == result.end());
+    }
+
+    SECTION("Test a successful range query not on entity") {
+        Eigen::Vector2d center(10.0, 10.0);
+        float radius = 30;
+        auto result = tree.norm1_range_query(center, radius);
+
+        REQUIRE(std::find(result.begin(), result.end(), ent_1) == result.end());
+        REQUIRE(std::find(result.begin(), result.end(), ent_2) == result.end());
+        REQUIRE(std::find(result.begin(), result.end(), ent_3) == result.end());
+        REQUIRE(std::find(result.begin(), result.end(), ent_4) == result.end());
+        REQUIRE(std::find(result.begin(), result.end(), ent_5) == result.end());
+        // REQUIRE(std::find(result.begin(), result.end(), ent_6) != result.end());
+        REQUIRE(std::find(result.begin(), result.end(), ent_7) == result.end());
+        REQUIRE(std::find(result.begin(), result.end(), ent_8) == result.end());
+        // REQUIRE(std::find(result.begin(), result.end(), ent_9) != result.end());
+    }
+
+    SECTION("Test an empty range query") {
+        Eigen::Vector2d center(20.0, 20.0);
+        float radius = 5.9;  // Closest point is ent_6
+        auto result = tree.norm1_range_query(center, radius);
+
+        REQUIRE(result.size() == 0);
     }
 }
