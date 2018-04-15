b9d01612bea1b04198a2c53509774bf1d4b25921
Gerry Agbobada <gagbobada+git@gmail.com>
2018-04-15 22:54:01 +0200

    Fix a mistake in time_step computation that made us choose bad constants

--- src/entity/entity.h
+++ src/entity/entity.h
@@ -154,7 +154,7 @@ class Entity {
     // Mass of the entity
     float mass{1.0};
     // Maximum acceleration possible for Entity
-    float max_acceleration{5e-4};
+    float max_acceleration{5e1};
     // Coefficient of friction (drag)
     float friction_factor{0};
     // Color used to display entity (in hex RGBA)
